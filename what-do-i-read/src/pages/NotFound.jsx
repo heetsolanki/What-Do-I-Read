cd58c57995fa9ef2bccc5a3cc798b057a0404e34
Heet Solanki <heetsolanki2006@gmail.com>
2025-10-06 20:06:32 +0530

    Updated database to REST API

--- what-do-i-read/src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound(){
-  return (
-    <div className="card" style={{padding:18}}>
-      <h2>404</h2>
-      <p className="muted">Page not found.</p>
-      <a className="btn" href="/">Go Home</a>
-    </div>
-  )
-}
